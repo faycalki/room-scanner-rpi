f17365d6a7d6742bb6228ddd95873a94ef018085
faycalki <fkilali@mta.ca>
2024-12-21 02:11:39 -0400

    Refactor audio handling to support MP3 and use pygame mixer.

--- src/audio.py
+++ src/audio.py
@@ -8,11 +8,19 @@
 #wget https://github.com/adafruit/Raspberry-Pi-Installer-Scripts/raw/main/i2samp.py
 #sudo -E env PATH=$PATH python3 i2samp.py
 
-from gtts import gTTS
+import gtts
+#from gtts import gTTS
 import os
-from playsound import playsound
+#import vlc
+#import subprocess
+#from playsound import playsound
+#import simpleaudio as sa
+#from pydub import AudioSegment
+#from pydub.playback import play
+#from audioplayer import AudioPlayer
+from pygame import mixer
 
-output = os.path.relpath("uploads/output.wav")
+output = os.path.relpath("uploads/output.mp3")
 
 def synthesize_audio(text, language):
     """
@@ -23,19 +31,37 @@ def synthesize_audio(text, language):
     """
     try:
         # Generate the audio
-        tts = gTTS(text=text, lang=language)
+        tts = gtts.gTTS(text=text, lang=language)
         output_file = output
         tts.save(output_file)
 
         # Play the audio
-        playsound(output_file)
+        #p = vlc.MediaPlayer("file:///" + output_file)
+        #p.play()
+        mixer.init()
+        mixer.music.load(output_file)
+        mixer.music.play()
+        # Wait for the sound to finish playing
+        while mixer.music.get_busy():
+            continue  # Loop until the audio is finished playing
+
+        #AudioPlayer(output_file).play(block=True)
+        #sound = AudioSegment.from_wav(output_file)
+        #play(sound)
+        #subprocess.run(['aplay', output_file], check=True)
+        #wave_obj = sa.WaveObject.from_wav_file(output_file)
+        #play_obj = wave_obj.play()
+        #play_obj.wait_done()
+        #playsound(output_file)
 
         # Optional: Remove the file after playback
-        os.remove(output_file)
+        #os.remove(output_file)
 
     except Exception as e:
         print(f"An error occurred: {e}")
-
+    finally:
+        mixer.quit()
+        #pass
 
 # Example usage
 if __name__ == "__main__":
